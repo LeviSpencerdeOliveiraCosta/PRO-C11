a2319c37c84c832cda470e732771b9191a90ffc8
LeviSpencerdeOliveiraCosta <105869343+LeviSpencerdeOliveiraCosta@users.noreply.github.com>
2022-06-01 12:12:07 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+
+function preload(){
+pathImg = loadImage("path.png");
+boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+ createCanvas(400,400);
+
+// Movendo o fundo 
+velocityY = -4
+if (condition) {
+    
+}
+
+
+//Criando menino que corre 
+runner1.addAnimation
+
+
+// Criando Boundary (Limite) esquerdo  
+leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible = false;
+
+
+//Crie Boundary direito 
+rightBoundary=createSprite(390,390,100,800)
+rightBoundary.visible = false
+}
+
+
+function draw() {
+background(0);
+path.velocityY = 4;
+
+// Menino se movendo no eixo X com o mouse
+edges= createEdgeSprites();
+boy.collide(edges[3]);
+
+
+
+ //Reiniciar o fundo
+ 
+
+
+
+ drawSprites();
+boy.worldX
+ }
+
+
+
+
